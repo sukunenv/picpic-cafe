91382eca6e94aa41757a84bf82579c7e63ba1e2d
KAL347 <email-kalpin347@gmail.com>
2026-04-13 17:21:38 +0700

    fix: update CORS for production deployment

--- config/cors.php
+++ config/cors.php
@@ -20,14 +20,11 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'https://picpic-cafe-web.netlify.app',
+        'https://picpic-cafe-admin.vercel.app',
         'http://localhost:5173',
-        'http://localhost:5174', 
+        'http://localhost:5174',
         'http://localhost:5175',
-        'http://127.0.0.1:5175',
-        'http://localhost:3000',
-        'http://localhost:5176',
-        'http://localhost:5177',
-        'http://localhost:5178',
     ],
 
     'allowed_origins_patterns' => [],
@@ -38,6 +35,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
