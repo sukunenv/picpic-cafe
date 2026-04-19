2cc8d7815e8efae729264eeb79890a9d184f6d28
Kalify.dev <kalpin347@gmail.com>
2026-04-20 04:21:25 +0700

    cleanup: remove temporary migration route

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// HAPUS SETELAH DIPAKAI!
-Route::get('/run-migration-seeder-picpic', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--class' => 'UpdateCategoryTypesSeeder', '--force' => true]);
-    $categories = \Illuminate\Support\Facades\DB::table('categories')->select('id','name','type')->get();
-    $output = '';
-    foreach ($categories as $c) {
-        $output .= $c->name . ' => ' . $c->type . "\n";
-    }
-    return '<pre>' . $output . '</pre>';
-});
