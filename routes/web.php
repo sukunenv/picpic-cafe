5bb09da809b09a7271c8dd411e5c07f8ef14366e
Kalify.dev <kalpin347@gmail.com>
2026-04-24 04:32:15 +0700

    chore: hapus temporary route run-migrate

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/run-migrate', function () {
-    if (app()->environment('production')) {
-        Artisan::call('migrate', ['--force' => true]);
-        return 'Migration done: ' . Artisan::output();
-    }
-    return 'Not allowed';
-});
