c225446fc7c90ad63bce5ca6598ae8c68ebc8ec7
Kalify.dev <kalpin347@gmail.com>
2026-04-24 04:29:16 +0700

    temp: tambah route run-migrate

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrate', function () {
+    if (app()->environment('production')) {
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Migration done: ' . Artisan::output();
+    }
+    return 'Not allowed';
+});
